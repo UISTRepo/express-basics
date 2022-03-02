34391b1fc924aef3c56215a9463081d2a8d6731b
tuntev <tuntev@gmail.com>
2022-03-02 15:33:03 +0100

    Added the base header/footer structure Added the home page layout Added some small design for the home page

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,46 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    let movies = [
+        {
+            id: 1,
+            title: 'Movie 1',
+            actor: 'Actor 1',
+            time: 'Time 1'
+        },
+        {
+            id: 2,
+            title: 'Movie 2',
+            actor: 'Actor 2',
+            time: 'Time 2'
+        },
+        {
+            id: 3,
+            title: 'Movie 3',
+            actor: 'Actor 3',
+            time: 'Time 3'
+        }
+    ];
+
     res.render('index', {
         title: 'Movies',
-        name: 'Naum'
+        currentPage: 'home',
+        movies: movies
     });
 });
 
-router.get('/:id', function(req, res, next) {
-    res.json({
-        id: 1,
-        name: "Mike"
+router.get('/about', function(req, res, next) {
+    res.render('about', {
+        title: 'About Us',
+        currentPage: 'about'
+    });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', {
+        title: 'Contact Us',
+        currentPage: 'contact'
     });
 });
 
